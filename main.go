c0650757b738df46440d88bc5e96dcb5a471f74b
Kesit Budi Kusumo <kesit@kernel.id>
2024-05-27 20:33:39 +0700

    feat: add recursive solution

--- main.go
+++ main.go
@@ -3,3 +3,20 @@ package main
 func main() {
 
 }
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	result := inorderTraversal(root.Left)
+	result = append(result, root.Val)
+	result = append(result, inorderTraversal(root.Right)...)
+	return result
+}
